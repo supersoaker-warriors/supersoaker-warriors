384a3d17e5ce0f8a36f8ad8067148307573b4257
Mikhail <mishnade@gmail.com>
2015-07-20 19:01:19 -0700

    [CREATE] Trying to test API, getting 404

--- Server/server.js
+++ Server/server.js
@@ -1,20 +1,63 @@
 var express = require("express");
 var bodyParser = require('body-parser');
 var db = require('./db.js');
-
-
+var path = require('path');
 
 var app = express();
-
-app.use(express.static(__dirname+ '/../Public'));
 
 app.use(bodyParser.json());  
 
 var PORT = 3000;
 
+// var UserSchema = new Schema ({
+//   id: ObjectId,
+//   username: String,
+//   first: String,
+//   last: String,
+//   email: String,
+//   password: String,
+//   age: Number,
+//   description: String ,
+//   profile_doodle: Object,
+//   backup_doodle: Object,
+//   date: Date
+
+
+// });
+
 app.get('/', function (req, res) {
-  res.send('Supersoaker Wariors!');
+  console.log(__dirname);
+  res.sendFile(path.resolve(__dirname + '/../Public/index.html'));
 });
+
+
+app.post('/api/new', function (req, res) {
+  console.log("post request received");
+  console.log(req.body);
+  var newUser = db({
+      id: ObjectId,
+      username: req.body.username,
+      first: req.body.first,
+      last: req.body.last,
+      email: req.body.email,
+      password: req.body.password,
+      age: req.body.age,
+      description: req.body.description,
+      profile_doodle: req.body.profile_doodle,
+      backup_doodle: req.body.backup_doodle
+  })
+  newUser.save(function (err, userObj) {
+    if (err) {
+      console.log("error creating user: ", err);
+      res.send('user saved successfully');
+    }
+    else {
+      console.log('user saved successfully: ', userObj);
+    }
+
+  })
+});
+app.use(express.static(__dirname+ '/../Public'));
 
 var server = app.listen(PORT, function () {
   var host = server.address().address;
@@ -23,6 +66,7 @@ var server = app.listen(PORT, function () {
   console.log("Server listening at http://%s:%s", host, port);
 
 });
+
 
 var saveUser = function () {
   console.log("saveUser called");
@@ -41,7 +85,7 @@ var saveUser = function () {
   });
 
 };
-saveUser();
+//saveUser();
 
 var findAllUsers = function () {
 
@@ -57,4 +101,4 @@ var findAllUsers = function () {
 
 
 )};
-findAllUsers();
+// findAllUsers();
