be52e49a4e05853b250c40fb4cb3f2b62156e46c
Patrick Yeung <pbbyeung@gmail.com>
2015-07-21 17:32:56 -0700

    [FIX] draw on canvas

--- client/app/app.js
+++ client/app/app.js
@@ -1,32 +1,13 @@
 //app.js
 angular.module('okdoodle', [
+
   'okdoodle.profile',
   'okdoodle.draw',
   'okdoodle.signin',
   'ui.router'
 ])
 .config(function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/draw')
-  // $routeProvider
-  //   .when('/signin', {
-  //     templateUrl: 'app/auth/signin.html',
-  //     controller: 'AuthController'
-  //   })
-  //   // Your code here
-  //   .when('/profile', {
-  //     templateUrl: 'app/profile/profile.html',
-  //     controller: 'ProfileController',
-  //     authenticate: true,
-  //   })
-  //   .when('/draw', {
-  //     templateUrl: 'app/draw/draw.html',
-  //     controller: 'DrawController',
-  //     authenticate: true,
-  //   })
-  //   .otherwise({
-  //     redirectTo: '/profile'
-  //   });
-
+  $urlRouterProvider.otherwise('/draw');
   $stateProvider
     .state('signin', {
       url: '/signin',
@@ -38,7 +19,7 @@ angular.module('okdoodle', [
       templateUrl: './app/profile/profile.html',
       controller: "ProfileController as profile"
     })
-    .state('profile.draw', {
+    .state('draw', {
       url: '/draw',
       templateUrl: './app/draw/draw.html',
       controller: 'DrawController as draw'
