1f23af85a4c221e3ca65951fd2eda39a209f4886
pbbyeung <pbbyeung@gmail.com>
2015-07-27 16:38:39 -0700

    [FIX] hard coded

--- client/app/browse/browse.js
+++ client/app/browse/browse.js
@@ -52,4 +52,76 @@ angular.module('okdoodle.browse', ['ui.router'])
   return {
     link: miniRender
   };
-}]);
+}])
+.directive('canvasBrowseTwo', ['$document', function($document) {
+  // function that takes care of drawing on canvas
+  function miniRender(scope, element, attrs) {
+    var pixelSize = 16;
+    var context = element[0].getContext('2d');
+    var xBitStart;
+    var yBitStart;
+    //THIS SCOPE IS NOT MODULAR.
+    console.log('scope.browse ', scope.browse);
+    var doodle = scope.browse.user.drawings[1].doodleArray[0];
+    render();
+
+    function render(){
+      console.log("minirender called", scope);
+      for(var thing in doodle){
+        var newThang = thing.split(",");
+        var newX = newThang[0].slice(1);
+        var newY = newThang[1].slice(0, newThang[1].length-1);
+        newX = parseInt(newX);
+        newY = parseInt(newY);
+        draw(newX, newY, doodle[thing]);
+      }
+    }
+    // canvas methods that draw from point to point
+    function draw(x, y, shade) {
+      xBitStart = x * pixelSize;
+      yBitStart = y * pixelSize;
+      context.fillStyle = "#" + shade;
+      // this starts a rectangle at the current X and Y, with a size of "pixelSize"
+      context.fillRect(xBitStart, yBitStart, pixelSize, pixelSize);
+    }
+  }
+  return {
+    link: miniRender
+  };
+}])
+.directive('canvasBrowseThree', ['$document', function($document) {
+  // function that takes care of drawing on canvas
+  function miniRender(scope, element, attrs) {
+    var pixelSize = 16;
+    var context = element[0].getContext('2d');
+    var xBitStart;
+    var yBitStart;
+    //THIS SCOPE IS NOT MODULAR.
+    console.log('scope.browse ', scope.browse);
+    var doodle = scope.browse.user.drawings[2].doodleArray[0];
+    render();
+
+    function render(){
+      console.log("minirender called", scope);
+      for(var thing in doodle){
+        var newThang = thing.split(",");
+        var newX = newThang[0].slice(1);
+        var newY = newThang[1].slice(0, newThang[1].length-1);
+        newX = parseInt(newX);
+        newY = parseInt(newY);
+        draw(newX, newY, doodle[thing]);
+      }
+    }
+    // canvas methods that draw from point to point
+    function draw(x, y, shade) {
+      xBitStart = x * pixelSize;
+      yBitStart = y * pixelSize;
+      context.fillStyle = "#" + shade;
+      // this starts a rectangle at the current X and Y, with a size of "pixelSize"
+      context.fillRect(xBitStart, yBitStart, pixelSize, pixelSize);
+    }
+  }
+  return {
+    link: miniRender
+  };
+}])
